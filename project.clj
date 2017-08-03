2240e40c0f286a954ad1e874f4174c43176ab74a
Changqi Chen <ccq777@gmail.com>
2017-08-03 16:18:00 -0400

    add readme && init project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject codeitlater "0.1.0"
+  :description "Track bread crumbs left in code."
+  :url "https://github.com/ccqpein/codeitlater"
+  :license {:name "Apache License"
+            :url "https://github.com/ccqpein/codeitlater/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
