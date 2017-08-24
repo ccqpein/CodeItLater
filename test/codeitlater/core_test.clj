c844be5cfc232838eecd5038e34e98db0a58595f
ccQpein <ccqpein@protonmail.com>
2017-08-24 08:12:19 -0400

    find a new bug

--- test/codeitlater/core_test.clj
+++ test/codeitlater/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [codeitlater.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest read-comment-inline-test
+  (testing "Read comment in line fail"
+    (is (= "// abc"
+            (read-comments-inline (make-pattern "//") "aaa // abc")))))
