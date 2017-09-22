4f533ddece7afb34232cb90a96ca6dd07126a141
ccQpein <ccqpein@protonmail.com>
2017-09-22 11:47:17 -0400

    fix println bug

---
+++ src/codeitlater/format.clj
@@ -0,0 +1,16 @@
+(ns codeitlater.format
+  )
+
+(defn list2tree [ls]
+  "make list to tree"
+  (loop [listset ls]
+    (if (not (empty? listset))
+          (let [thisls (first listset)
+                [filepath & tuples] thisls]
+            (printf "|-- %s\n" filepath)
+            (doseq [tuple tuples]
+              (printf "  |-- %s\n" tuple))
+            (recur (rest listset))
+            )
+          )))
+
