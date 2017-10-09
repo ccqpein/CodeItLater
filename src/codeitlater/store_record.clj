69e88d17279500a1a0ca02ad915acbeab7b92a08
ccQpein <ccqpein@protonmail.com>
2017-10-09 16:19:58 -0400

    add a bit

---
+++ src/codeitlater/store_record.clj
@@ -0,0 +1,11 @@
+(ns codeitlater.store-record
+  )
+
+(defn write-down [path lst]
+  "store list result returned from core/read-file to local path"
+  )
+
+(defn read-from [path]
+  "read lst back from local file")
+
+
