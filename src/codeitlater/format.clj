99b86c1c2e6c59a1c585929449eac978e05761a7
ccQpein <ccqpein@protonmail.com>
2018-01-11 08:34:50 -0500

    fix print file path even no mark word comment in file

--- src/codeitlater/format.clj
+++ src/codeitlater/format.clj
@@ -1,20 +1,29 @@
 (ns codeitlater.format
   )
 
+(defn printline [filepath tuples]
+  (do
+    (printf "|-- %s\n" filepath)
+    (doall (map #(printf "  |-- %s\n" %) tuples))))
+
+(defn print-with-keyword [filepath tuples keyword]
+  (let [filter-content (filter
+                        #(-> (str "(?<=" keyword ":" ").*$") (re-pattern) (re-find (second %)))
+                        tuples)]
+    (if (not (empty? filter-content))
+      (printline filepath filter-content)
+      )))
+
 (defn list2tree [ls keyword]
   "make list to tree"
   (loop [listset ls]
     (if (not (empty? listset))
           (let [thisls (first listset)
                 [filepath & tuples] thisls]
-            (printf "|-- %s\n" filepath)
-            (doseq [tuple tuples]
-              (if keyword
-                (let [content (-> (str "(?<=" keyword ":" ").*$") (re-pattern) (re-find (second tuple)))]
-                  (if content
-                    (printf "  |-- %s\n" tuple)))
-                (printf "  |-- %s\n" tuple)))
+            ;;:= TODO: make function to fix empty keyword issue
+            (if keyword
+              (print-with-keyword filepath tuples keyword)
+              (printline filepath tuples))
             (println)
             (recur (rest listset))
             ))))
-
