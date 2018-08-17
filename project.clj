3f4f9e065dab16900c399f3f0821afd12726f519
Changqi Chen <ccq777@gmail.com>
2018-08-17 14:17:58 -0400

    a bit change

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ccqpein/codeitlater"
   :license {:name "Apache License"
             :url "https://github.com/ccqpein/codeitlater/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.cli "0.3.5"]]
   :main codeitlater.core)
