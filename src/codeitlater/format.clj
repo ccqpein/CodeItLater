548ab8785a87d2625ef852d869986d1cb1e4a606
ccQpein <ccqpein@protonmail.com>
2018-09-27 19:00:27 -0400

    a bit

--- src/codeitlater/format.clj
+++ src/codeitlater/format.clj
@@ -1,5 +1,6 @@
 (ns codeitlater.format
   (:require [clojure.string :as str])
+  (:require [clojure.java.io :as io])
   )
 
 (defn printline [filepath tuples]
@@ -18,12 +19,15 @@
       (printline filepath filter-content)
       )))
 
-(defn format-keyword-content [filepath tuples keyword]
-  "cut content those including keyword"
+(defn format-keyword-content [tuples keyword]
+  "cut content those including keyword:
+return ((linenum [keyword content])..)
+"
   (let [filter-content (filter #(-> (str "(?<=" keyword ":" ").*$")
                                     (re-pattern)
                                     (re-find (second %)))
                                tuples)]
+    ;;(print filter-content)
     (if (not (empty? filter-content))
       (doall (map #(list (first %) (str/split (second %) #": ")) filter-content))
       )))
@@ -34,24 +38,30 @@
   ;;(println ls)
   (loop [listset ls]
     (if (not (empty? listset))
-          (let [thisls (first listset)
-                [filepath & tuples] thisls]
-            (if keyword
-              (print-with-keyword filepath tuples keyword)
-              (printline filepath tuples)
-              )
-            ;(println)
-            (recur (rest listset))
-            ))))
+      (let [thisls (first listset)
+            [filepath & tuples] thisls]
+        (if keyword
+          (print-with-keyword filepath tuples keyword)
+          (printline filepath tuples)
+          )
+        ;;(println)
+        (recur (rest listset))
+        ))))
 
-;;;:= TODO: need to clean ':' after keyword
-(defn write-keyword-sentence [level content]
+(defn write-keyword-sentence [ls keyword path]
   "write keyword content in level"
-  (let [this_level_mark (reduce str
-                                (take (inc level)
-                                      (repeat "*")))]
-    ;;(print this_level_mark)
-    (doall (map #(str this_level_mark
-                      " "
-                      (second %))
-                content))))
+ (with-open [wrtr (io/writer path)]
+  (loop [listset ls]
+    (if (not (empty? listset))
+      (let [thisls (first listset)
+            [filepath & tuples] thisls]
+        ;(.write wrtr tuples)
+        (.write wrtr (str "*" filepath "\n"
+                          (format-keyword-content tuples keyword)))
+        (recur (rest listset)))))))
+
+
+(defn write-to-file [path]
+  (with-open [wrtr (io/writer path)]
+    (.write wrtr "test")
+    (.write wrtr "test1")))
